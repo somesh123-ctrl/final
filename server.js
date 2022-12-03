4a75ee6094fc007daa522df054e3ec09c9c35a72
somesh <someshshirpe123@gmail.com>
2022-12-03 13:31:33 +0530

    version3

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ app.use(houseListingRoute);
 app.use(houseFetchRoute);
 app.use(ContactFormRoute);
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
